20e38e60ea411f01c0c54ba992c6f1570aba8f87
chako <chakib.aitsaada@gmail.com>
2024-10-26 12:53:03 +0100

    adding all

--- websiteview/admin.py
+++ websiteview/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import machines,machine_type,machine_data
+
+
+admin.site.register(machine_data)
+admin.site.register(machine_type)
+admin.site.register(machines)
